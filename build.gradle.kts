8cbdacf66af00e1ea23da180fd2d4429bf40a766
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2023-12-19 23:20:38 +0100

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.20"
+    id("org.jetbrains.intellij") version "1.16.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2023.1.4")
+    type.set("PY") // Target IDE Platform
+
+    plugins.set(listOf("Pythonid"))
+    downloadSources.set(false)
+}
+
+tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
+
+    patchPluginXml {
+        version.set("${project.version}")
+        sinceBuild.set("231")
+        untilBuild.set("233.*")
+    }
+}
---
+++ src/main/java/org/example/demo/ExplainMethodAction.java
@@ -0,0 +1,13 @@
+package org.example.demo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class ExplainMethodAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        // Extract the method text of the currently selected line
+
+    }
+}
